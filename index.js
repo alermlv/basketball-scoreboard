bb5411fabfe7638c817730c4cfdf1d220e07a6cd
alermlv <102925147+alermlv@users.noreply.github.com>
2024-06-13 14:19:10 +0300

    Add addPoints and updateScore functions

--- index.js
+++ index.js
@@ -4,6 +4,16 @@ const addPointsButtons = document.querySelectorAll(".add-points-button");
 let homePoints = 0;
 let awayPoints = 0;
 
-function addPoints() {
-  console.log("clicked")
+function addPoints(obj) {
+  if (obj.classList.contains("home-button")) {
+    homePoints += +obj.value;
+  } else {
+    awayPoints += +obj.value;
+  }
+  updateScore();
+}
+
+function updateScore() {
+  homeDisplay.textContent = homePoints;
+  awayDisplay.textContent = awayPoints;
 }
