5e5a954405440f79dc32810b7cff66eebea4387f
alermlv <102925147+alermlv@users.noreply.github.com>
2024-06-13 13:31:02 +0300

    Add variables for display, buttons and scores

---
+++ index.js
@@ -0,0 +1,4 @@
+const scoreboardDisplayElements = document.querySelectorAll(".scoreboard-display");
+const addScoreButtons = document.querySelectorAll(".add-score-button");
+let homeScoreValue = 0;
+let awayScoreValue = 0;
